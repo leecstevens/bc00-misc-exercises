37dd1d9b2b9b42b9c33d2ce45307b0f9a4c8e451
lee <lee@stevensnet.com>
2023-05-22 21:15:18 -0500

    add last of wk3 Monday

--- wk03/wk03-ex08/Unsolved/script.js
+++ wk03/wk03-ex08/Unsolved/script.js
@@ -8,12 +8,12 @@ var expression1 = (b === e);
 var expression2 = (e < d);
 
 // Use comparison operators so all expressions below log to the console as true
-console.log(a === b);
-console.log(b !== e);
-console.log(c < b);
-console.log(d > 0);
+console.log(a == b); // Remove extra =
+console.log(b == e); // Remove !
+console.log(c > b); // Flip gt
+console.log(d == 0); // d != < || >
 
 // Use logical operators so all expressions below log to the console as true
-console.log(expression1 && expression2);
-console.log( !expression1 || expression2);
+console.log(expression1 && !expression2);  // Add ! to expression2
+console.log( expression1 || expression2); 
   
