e062f534386977787cc9e2b00ddc6a6805ab9792
lee <lee@stevensnet.com>
2023-05-24 19:31:18 -0500

    wk03-ex14

--- wk03/wk03-ex14/Unsolved/script.js
+++ wk03/wk03-ex14/Unsolved/script.js
@@ -1,1 +1,20 @@
 //WRITE YOUR CODE BELOW
+const greeting = "Great to see you, "
+students = ["Valerie", "Iz", "Lee", "Stuart", "Brad"]
+
+for (let i=0; i < students.length; i++) {
+    console.log(`(loop): ${greeting}${students[i]}!`)
+}
+
+i = 0;
+while (i < students.length) {
+    console.log(`(while): ${greeting}${students[i]}!`)
+    i++;
+}
+
+i = 0;
+do {
+    console.log(`(do): ${greeting}${students[i]}!`)  
+    i++;
+}
+while(i < students.length)
